bd7aabb8801cdce7e127cf4a7aa9e3b4a32d31ef
Simon Wahlin <simon.wahlin@knowledgefactory.se>
2018-02-06 06:47:58 +0100

    Added Git-SID function

--- Private/LDAPObject.ps1
+++ Private/LDAPObject.ps1
@@ -44,3 +44,29 @@ function Get-LDAPObject {
         throw
     }
 }
+
+function Get-SID {
+    [CmdletBinding()]
+    param (
+        # DistinguishedName of LDAP object to get SID from
+        [Parameter(Mandatory)]
+        [string]
+        $DistinguishedName,
+
+        # Set domain controller to use
+        [Parameter()]
+        [string]
+        $Server,
+
+        # Set Credentials to use when connecting
+        [Parameter()]
+        [pscredential]
+        $Credential
+    )
+
+    process {
+        $Object = Get-LDAPObject @PSBoundParameters
+        New-Object -TypeName System.Security.Principal.SecurityIdentifier -ArgumentList $Object.ObjectSID.Value, 0
+    }
+
+}
