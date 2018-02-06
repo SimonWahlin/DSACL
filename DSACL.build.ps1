639de35597cc6c55ed6afaa2085f1cec02739ca6
Simon Wahlin <simon.wahlin@knowledgefactory.se>
2018-02-06 06:44:38 +0100

    Renamed build script

--- DSACL.build.ps1
+++ DSACL.build.ps1
@@ -1,13 +1,10 @@
 #Requires -Modules @{ModuleName='InvokeBuild';ModuleVersion='3.2.1'}
 #Requires -Modules @{ModuleName='PowerShellGet';ModuleVersion='1.6.0'}
 
-$script:IsAppveyor = $env:APPVEYOR -ne $null
-$script:ModuleName = Get-Item -Path $BuildRoot | Select-Object -ExpandProperty Name
+$Script:IsAppveyor = $env:APPVEYOR -ne $null
+$Script:ModuleName = Get-Item -Path $BuildRoot | Select-Object -ExpandProperty Name
 Get-Module -Name $ModuleName | Remove-Module -Force
-
-if ($PSVersionTable.PSEdition -ne "Core") {
-    Add-Type -Assembly System.IO.Compression.FileSystem
-}
+Import-Module "$BuildRoot\buildhelpers\helpers.psm1"
 
 task Clean {
     Remove-Item -Path ".\Bin" -Recurse -Force -ErrorAction SilentlyContinue
@@ -15,7 +12,7 @@ task Clean {
 
 task TestCode {
     Write-Build Yellow "`n`n`nTesting dev code before build"
-    $TestResult = Invoke-Pester -PassThru
+    $TestResult = Invoke-Pester -Script "$PSScriptRoot\test\Unit" -Tag Unit -PassThru
     if($TestResult.FailedCount -gt 0) {throw 'Tests failed'}
 }
 
@@ -26,20 +23,14 @@ task CopyFiles {
 }
 
 task CompilePSM {
-    param(
-
-    )
     $PrivatePath = '{0}\Private\*.ps1' -f $BuildRoot
     $PublicPath = '{0}\Public\*.ps1'-f $BuildRoot
     $ScriptPath = '{0}\Script\*.ps1'-f $BuildRoot
-    $Scripts = Get-ChildItem -Path $ScriptPath,$PrivatePath,$PublicPath |
-        Select-Object -ExpandProperty FullName
-    Get-Content -Path $Scripts |
-        Out-File -FilePath "$BuildRoot\bin\$ModuleName\$ModuleName.psm1" -Encoding UTF8 -Force
+    Merge-ModuleFiles -Path $ScriptPath,$PrivatePath,$PublicPath -OutputPath "$BuildRoot\bin\$ModuleName\$ModuleName.psm1"
 
-    $PublicScriptBlock = [ScriptBlock]::Create((Get-ChildItem -Path $PublicPath | Get-Content | Out-String))
-    $PublicFunctions = $PublicScriptBlock.Ast.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst]},$false).Name
-    $PublicAlias = $PublicScriptBlock.Ast.FindAll({ $args[0] -is [System.Management.Automation.Language.AttributeAst] },$true).Where{$_.TypeName.FullName -eq 'Alias' -and $_.Parent -is [System.Management.Automation.Language.ParamBlockAst]}.PositionalArguments.Value
+    $PublicScriptBlock = Get-ScriptBlockFromFile -Path $PublicPath
+    $PublicFunctions = Get-FunctionFromScriptblock -ScriptBlock $PublicScriptBlock
+    $PublicAlias = Get-AliasFromScriptblock -ScriptBlock $PublicScriptBlock
     $PublicFunctionParam, $PublicAliasParam = ''
     $UpdateManifestParam = @{}
     if(-Not [String]::IsNullOrEmpty($PublicFunctions)) {
@@ -57,7 +48,8 @@ task CompilePSM {
         $gitversion = gitversion | ConvertFrom-Json
         $UpdateManifestParam['ModuleVersion'] = $gitversion.MajorMinorPatch
         if ($gitversion.CommitsSinceVersionSource -gt 0) {
-            # Prerelease
+            # Prerelease, raise minor-version by 1 and add prerelease string.
+            $UpdateManifestParam['ModuleVersion'] = '{0}.{1}.{2}' -f $gitversion.Major, ($gitversion.Minor+1), $gitversion.Patch
             $UpdateManifestParam['Prerelease'] = '-beta{0}' -f $gitversion.CommitsSinceVersionSourcePadded
         }
     }
@@ -72,14 +64,29 @@ task MakeHelp -if (Test-Path -Path "$PSScriptRoot\Docs") {
 
 task TestBuild {
     Write-Build Yellow "`n`n`nTesting compiled module"
-    $TestResult = Invoke-Pester -Script  @{Path="$PSScriptRoot\test"; Parameters=@{ModulePath="$BuildRoot\bin\$ModuleName"}} -Tag CoreModule -PassThru
+    $Script =  @{Path="$PSScriptRoot\test\Unit"; Parameters=@{ModulePath="$BuildRoot\bin\$ModuleName"}}
+    $CodeCoverage = Get-Module "$BuildRoot\bin\$ModuleName" -ListAvailable |
+        Select-Object -ExpandProperty ExportedCommands |
+        Select-Object -ExpandProperty Keys | Foreach-Object -Process {
+            @{Path="$BuildRoot\bin\$ModuleName\$ModuleName.psm1";Function=$_}
+        }
+    $TestResult = Invoke-Pester -Script $Script -Tag Unit -CodeCoverage $CodeCoverage -PassThru
     if($TestResult.FailedCount -gt 0) {throw 'Tests failed'}
 }
 
-task Build CopyFiles, CompilePSM, MakeHelp, TestBuild
+task Sign -if ($ShoudSign) {
+
+}
 
 task UploadArtifactsAppveyor -if ($script:IsAppveyor) {
     # Put code to upload artifacts to Appveyor here
 }
 
-task . Clean, TestCode, Build, UploadArtifactsAppveyor
+task PublishToGallery {
+
+}
+
+task . Clean, TestCode, Build, UploadArtifacts, PublishToGallery
+
+task Build CopyFiles, CompilePSM, MakeHelp, TestBuild, Sign
+task UploadArtifacts UploadArtifactsAppveyor
