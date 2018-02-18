5612e984c016b13497db4bb9bf71bbd77ac1b94d
Simon Wahlin <simon.wahlin@knowledgefactory.se>
2018-02-18 22:24:55 +0100

    Removed comments

--- Public/JoinDomain.ps1
+++ Public/JoinDomain.ps1
@@ -1,40 +1,4 @@
 <#
-    Access set by Arwidmark (Win 8.1)
-    https://deploymentresearch.com/Research/Post/353/PowerShell-Script-to-set-permissions-in-Active-Directory-for-OSD
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":CCDC;Computer" /I:T | Out-Null
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":LC;;Computer" /I:S | Out-Null
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":RC;;Computer" /I:S | Out-Null
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":WD;;Computer" /I:S  | Out-Null
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":WP;;Computer" /I:S  | Out-Null
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":RP;;Computer" /I:S | Out-Null
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Reset Password;Computer" /I:S | Out-Null
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Change Password;Computer" /I:S | Out-Null
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to service principal name;Computer" /I:S | Out-Null
-        dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to DNS host name;Computer" /I:S | Out-Null
-
-        CreateChild, DeleteChild                  - Computer
-        ListChildren                              - Computer
-        ReadSecurityInformation                   - Computer
-        ChangeSecurityInformation                 - Computer
-        WriteProperty                             - Computer
-        ReadProperty                              - Computer
-        Extended Reset Password                   - Computer
-        Extended Change Password                  - Computer
-        Validated write to DNS host name          - Computer
-        Validated write to service principal name - Computer
-
-    According to me:
-        •	Reset Password
-        •	Read and write to Account Restrictions
-        •	Validated write to DNS host name
-        •	Validated write to service principal name
-        Switches for
-        -AllowCreate
-        -AllowDelete
-
-#>
-
-<#
     .SYNOPSIS
     Give $DelegateDN rights to join computers in target (usually an OU).
 
