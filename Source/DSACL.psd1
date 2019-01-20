f52cc2048b05282e46b9782c3382a7b109debd57
Simon Wahlin <simon.wahlin@gmail.com>
2019-01-20 19:02:39 +0100

    Moved module code into Source folder

--- Source/DSACL.psd1
+++ Source/DSACL.psd1
@@ -63,13 +63,13 @@ Description = 'Active Directory ACLs - Delegation made easy'
 # TypesToProcess = @()
 
 # Format files (.ps1xml) to be loaded when importing this module
-# FormatsToProcess = @()
+FormatsToProcess = @('DSACL.format.ps1xml')
 
 # Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-# FunctionsToExport = @('*')
+FunctionsToExport = @('*')
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 # CmdletsToExport = @()
@@ -78,7 +78,7 @@ Description = 'Active Directory ACLs - Delegation made easy'
 # VariablesToExport = @()
 
 # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
-# AliasesToExport = @()
+AliasesToExport = @()
 
 # DSC resources to export from this module
 # DscResourcesToExport = @()
--- Source/DSACL.psm1
+++ Source/DSACL.psm1
@@ -1,15 +1,13 @@
 $ModulePath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-$PrivatePath = '{0}\Private\*.ps1' -f $ModulePath
-$PublicPath = '{0}\Public\*.ps1'-f $ModulePath
-$ScriptPath = '{0}\Script\*.ps1'-f $ModulePath
-$Scripts = Get-ChildItem -Path $ScriptPath,$PrivatePath, $PublicPath | Select-Object -ExpandProperty FullName
+$BuildData = Import-LocalizedData -BaseDirectory $ModulePath -FileName build.psd1
 
+$Scripts = Get-ChildItem -Path $BuildData.SourceDirectories -File -Filter *.ps1 | Select-Object -ExpandProperty FullName
 foreach($Script in $Scripts) {
         . $Script
 }
 $SearchRecursive = $true
 $SearchRootOnly  = $false
-$PublicScriptBlock = [ScriptBlock]::Create((Get-ChildItem -Path $PublicPath | Get-Content | Out-String))
+$PublicScriptBlock = [ScriptBlock]::Create((Get-ChildItem -Path $BuildData.PublicFilter | Get-Content -Delimiter ([char]0) | Out-String))
 $PublicFunctions = $PublicScriptBlock.Ast.FindAll({ $args[0] -is [System.Management.Automation.Language.FunctionDefinitionAst]},$SearchRootOnly).Name
 $PublicAlias = $PublicScriptBlock.Ast.FindAll({ $args[0] -is [System.Management.Automation.Language.ParamBlockAst] },$SearchRecursive).Where{$_.TypeName.FullName -eq 'alias'}.PositionalArguments.Value
 
